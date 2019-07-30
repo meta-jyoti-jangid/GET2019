a8bde317b18f2234eb471631b40c191092d68851
meta-jyoti-jangid <jyoti.jangid@metacube.com>
2019-07-30 17:41:56 +0530

    Assignment 9

---
+++ SCF/ASSIGNMENT9/Shape.java
@@ -0,0 +1,54 @@
+import java.util.Date;
+
+/**
+ * This interface is made for Shapes Properties
+ * 
+ * @author jyoti
+ *
+ */
+public interface Shape {
+
+	/**
+	 * Enum for shapes
+	 */
+	public enum Shapetype {
+		CIRCLE, RECTANGLE, SQUARE, TRIANGLE
+	};
+
+	/**
+	 * @return area
+	 */
+	public double getArea();
+
+	/**
+	 * @return type
+	 */
+	public Shapetype getType();
+
+	/**
+	 * @return perimeter
+	 */
+	public double getPerimeter();
+
+	/**
+	 * @return origin
+	 */
+	public Point getOrigin();
+
+	/**
+	 * @param p
+	 * @return point is inclosed or not
+	 */
+	public boolean isPointEnclosed(Point p);
+
+	/**
+	 * @return distance from origin
+	 */
+	public double getDistance();
+
+	/**
+	 * @return date and time
+	 */
+	public Date getTimeStamp();
+
+}
