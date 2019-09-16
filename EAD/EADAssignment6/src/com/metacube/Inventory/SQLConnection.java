d012bb15261b9f5cbf03d4d176b85fd00df48409
meta-jyoti-jangid <jyoti.jangid@metacube.com>
2019-09-16 15:43:03 +0530

    Assignment 6 of EAD

---
+++ EAD/EADAssignment6/src/com/metacube/Inventory/SQLConnection.java
@@ -0,0 +1,44 @@
+package com.metacube.Inventory;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Reader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import com.ibatis.common.jdbc.ScriptRunner;
+
+/**
+ * This is a component class. This class provides connection from the database
+ * 
+ * @author Admin
+ */
+public class SQLConnection {
+
+	// Reference variables.
+	private static Connection connection;
+
+	/**
+	 * This method returns Connection class object.
+	 * 
+	 * @return Connection class object
+	 */
+	public static Connection getConnection() {
+		String url = "jdbc:mysql://localhost:3306/";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(url, "root", "Jyoti@123");
+			// Initialize object for ScripRunner
+			ScriptRunner sr = new ScriptRunner(connection, false, false);
+			// Give the input file to Reader
+			Reader reader = new BufferedReader(new FileReader(
+					"C:/Users/Admin/workspace/EAD6/ElementsQuery.sql"));
+			// Exctute script
+			sr.runScript(reader);
+		} catch (Exception e) {
+			System.err.println("Failed to Execute" + " The error is "
+					+ e.getMessage());
+		}
+		return connection;
+	}
+}
